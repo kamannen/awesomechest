a9bef53c2658c1629e35ad0d33b5d0eec1278c51
kamannen <karlarne0@gmail.com>
2015-08-22 21:49:59 +0200

    Removed unused variables.

--- src/main/java/kamannen/awesomechest/container/ContainerAWChest.java
+++ src/main/java/kamannen/awesomechest/container/ContainerAWChest.java
@@ -10,16 +10,6 @@ import net.minecraft.item.ItemStack;
 
 public class ContainerAWChest extends Container {
 
-    private final int HOTBAR_SLOT_COUNT = 9;
-    private final int PLAYER_INVENTORY_ROW_COUNT = 3;
-    private final int PLAYER_INVENTORY_COLUMN_COUNT = 9;
-    private final int PLAYER_INVENTORY_SLOT_COUNT = PLAYER_INVENTORY_COLUMN_COUNT * PLAYER_INVENTORY_ROW_COUNT;
-    private final int VANILLA_SLOT_COUNT = HOTBAR_SLOT_COUNT + PLAYER_INVENTORY_SLOT_COUNT;
-
-    private final int VANILLA_FIRST_SLOT_INDEX = 0;
-    private final int TE_INVENTORY_FIRST_SLOT_INDEX = VANILLA_FIRST_SLOT_INDEX + VANILLA_SLOT_COUNT;
-    private final int TE_INVENTORY_SLOT_COUNT = 27;
-
     private final InventoryAWChest inventoryAWChest;
 
     public ContainerAWChest(final EntityPlayer player, final InventoryAWChest inventory) {
@@ -47,7 +37,6 @@ public class ContainerAWChest extends Container {
     @Override
     public void onContainerClosed(final EntityPlayer player) {
         super.onContainerClosed(player);
-//        inventoryAWChest.
     }
 
 
